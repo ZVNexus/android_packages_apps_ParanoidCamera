25abd6668cf64cff5309bbdf6e495df949da40ec
Thecrazyskull <anaskarbila@gmail.com>
2017-01-22 07:50:19 -0600

    Add support for grid mode.
    Change-Id: I7557bfd1dcb968bbf7ee17b13e66b8cf182e763e

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -627,6 +627,8 @@ public class PhotoModule
         mLocationManager = new LocationManager(mActivity, this);
         mSensorManager = (SensorManager)(mActivity.getSystemService(Context.SENSOR_SERVICE));
 
+        mUI.getCameraControls().setCameraActivity(mActivity);
+
         brightnessProgressBar = (ProgressBar)mRootView.findViewById(R.id.progress);
 
         mBokehTipText = (TextView) mRootView.findViewById(R.id.bokeh_tip_text);
@@ -646,6 +648,8 @@ public class PhotoModule
 
         // LGE HDR mode
         mLgeHdrMode = activity.getResources().getBoolean(R.bool.lge_hdr_mode);
+
+        mActivity.showGrid(mPreferences);
     }
 
     private void initializeControlByIntent() {
@@ -2899,6 +2903,7 @@ public class PhotoModule
             s.setListener(null);
         }
         mUI.removeDisplayChangeListener();
+        mActivity.showGrid(mPreferences);
     }
 
     /**
@@ -5065,6 +5070,7 @@ public class PhotoModule
             mHandler.sendEmptyMessage(SET_PHOTO_UI_PARAMS);
         }
         resizeForPreviewAspectRatio();
+        mActivity.showGrid(mPreferences);
     }
 
     @Override
