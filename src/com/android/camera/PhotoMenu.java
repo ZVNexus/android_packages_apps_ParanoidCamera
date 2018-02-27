AOSPA/android_packages_apps_SnapdragonCamera

Camera: add support for grid mode
Change-Id: I7557bfd1dcb968bbf7ee17b13e66b8cf182e763e

src/com/android/camera/PhotoMenu.java
@@ -193,7 +193,8 @@ public class PhotoMenu extends MenuController
                 CameraSettings.KEY_QC_CHROMA_FLASH,
                 CameraSettings.KEY_REDEYE_REDUCTION,
                 CameraSettings.KEY_SELFIE_MIRROR,
-                CameraSettings.KEY_SHUTTER_SOUND
+                CameraSettings.KEY_SHUTTER_SOUND,
+                CameraSettings.KEY_GRID
         };
 
         mOtherKeys2 = new String[] {
@@ -238,6 +239,7 @@ public class PhotoMenu extends MenuController
                 CameraSettings.KEY_MANUAL_FOCUS,
                 CameraSettings.KEY_SELFIE_MIRROR,
                 CameraSettings.KEY_SHUTTER_SOUND,
+                CameraSettings.KEY_GRID,
                 SettingsManager.KEY_CAMERA2
         };
 
@@ -1281,7 +1283,6 @@ public class PhotoMenu extends MenuController
         } else {
             initializePopup();
         }
-
     }
 
     @Override

ZVNexus/android_packages_apps_ParanoidCamera

src/com/android/camera/PhotoMenu.java
@@ -199,7 +199,8 @@ public class PhotoMenu extends MenuController
                 CameraSettings.KEY_QC_CHROMA_FLASH,
                 CameraSettings.KEY_REDEYE_REDUCTION,
                 CameraSettings.KEY_SELFIE_MIRROR,
-                CameraSettings.KEY_SHUTTER_SOUND
+                CameraSettings.KEY_SHUTTER_SOUND,
+                CameraSettings.KEY_GRID
         };
 
         mOtherKeys2 = new String[] {
@@ -246,7 +247,8 @@ public class PhotoMenu extends MenuController
                 CameraSettings.KEY_MANUAL_FOCUS,
                 CameraSettings.KEY_SELFIE_MIRROR,
                 CameraSettings.KEY_SHUTTER_SOUND,
-                CameraSettings.KEY_ZOOM
+                CameraSettings.KEY_ZOOM,
+                CameraSettings.KEY_GRID
         };
 
         initSwitchItem(CameraSettings.KEY_CAMERA_ID, mFrontBackSwitcher);
@@ -1358,7 +1360,6 @@ public class PhotoMenu extends MenuController
         } else {
             initializePopup();
         }
-
     }
 
     @Override