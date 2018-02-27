AOSPA/android_packages_apps_SnapdragonCamera

SnapdragonCamera: Allow using an absolute sharpness value
The sharpness value which can be configured normally
is not accurate for most camera HALs.
So, to allow fine tuning these, we introduce
"config_use_absolute_sharpness" which is disabled by
default.
If set to true the sharpness value in "pref_camera_sharpness_default"
is regarded as an absolute sharpness value and
gets passed to the HAL without the addtional
multiplications.

Change-Id: Idff4d5be11b8921e248fece25e1afcb19e9ffb0d
Signed-off-by: Alex Naidis <alex.naidis@linux.com>

src/com/android/camera/PhotoModule.java
@@ -627,9 +627,10 @@ public class PhotoModule
         Storage.setSaveSDCard(
             mPreferences.getString(CameraSettings.KEY_CAMERA_SAVEPATH, "0").equals("1"));
 
-        // LGE HDR mode
+        // Get Resources
         if (mApplicationContext != null) {
             mLgeHdrMode = mApplicationContext.getResources().getBoolean(R.bool.lge_hdr_mode);
+            mUseAbsoluteSharpness = mApplicationContext.getResources().getBoolean(R.bool.config_use_absolute_sharpness);
         }
 
         mActivity.showGrid(mPreferences);
@@ -3292,8 +3293,16 @@ public class PhotoModule
         String sharpnessStr = mPreferences.getString(
                 CameraSettings.KEY_SHARPNESS,
                 mActivity.getString(R.string.pref_camera_sharpness_default));
-        int sharpness = Integer.parseInt(sharpnessStr) *
-                (mParameters.getMaxSharpness()/MAX_SHARPNESS_LEVEL);
+
+        int sharpness = 0;
+
+        if (mUseAbsoluteSharpness) {
+                sharpness = Integer.parseInt(sharpnessStr);
+        } else {
+                sharpness = Integer.parseInt(sharpnessStr) *
+                        (mParameters.getMaxSharpness()/MAX_SHARPNESS_LEVEL);
+        }
+
         Log.v(TAG, "Sharpness value =" + sharpness);
         if((0 <= sharpness) && (sharpness <= mParameters.getMaxSharpness())){
             mParameters.setSharpness(sharpness);

ZVNexus/android_packages_apps_ParanoidCamera

src/com/android/camera/PhotoModule.java
@@ -144,6 +144,7 @@ public class PhotoModule
     private int mReceivedSnapNum = 0;
     public boolean mFaceDetectionEnabled = false;
     private boolean mLgeHdrMode = false;
+    private boolean mUseAbsoluteSharpness = false;
     private DrawAutoHDR mDrawAutoHDR;
    /*Histogram variables*/
     private GraphView mGraphView;
@@ -652,8 +653,8 @@ public class PhotoModule
         Storage.setSaveSDCard(
             mPreferences.getString(CameraSettings.KEY_CAMERA_SAVEPATH, "0").equals("1"));
 
-        // LGE HDR mode
         mLgeHdrMode = activity.getResources().getBoolean(R.bool.lge_hdr_mode);
+        mUseAbsoluteSharpness = activity.getResources().getBoolean(R.bool.config_use_absolute_sharpness);
 
         mActivity.showGrid(mPreferences);
     }
@@ -3520,6 +3521,10 @@ public class PhotoModule
                 mActivity.getString(R.string.pref_camera_sharpness_default));
         int sharpness = Integer.parseInt(sharpnessStr) *
                 (ParametersWrapper.getMaxSharpness(mParameters)/MAX_SHARPNESS_LEVEL);
+        if (mUseAbsoluteSharpness) {
+            sharpness = Integer.parseInt(sharpnessStr);
+        }
+
         Log.v(TAG, "Sharpness value =" + sharpness);
         if((0 <= sharpness) && (sharpness <= ParametersWrapper.getMaxSharpness(mParameters))){
             ParametersWrapper.setSharpness(mParameters, sharpness);