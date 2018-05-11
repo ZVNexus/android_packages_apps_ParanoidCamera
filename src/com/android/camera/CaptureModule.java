bb13e0b6cc8c3a605f61e929876bea55617fb499
junjiez <junjiez@codeaurora.org>
2018-05-11 14:39:08 +0800

    SnapdragonCamera:Fix NP in touch focus
    Avoid CropRegion to be null when caculate
    afaeRectangle
    
    Change-Id: I112ca01225ffe542b6f33e68c9aed687aba1c8f6
    CRs-Fixed: 2237113

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -7713,6 +7713,10 @@ public class CaptureModule implements CameraModule, PhotoController,
             mInTAF = false;
             return;
         }
+        if (mCropRegion[id] == null) {
+            Log.d(TAG, "crop region is null at " + id);
+            return;
+        }
         Point p = mUI.getSurfaceViewSize();
         int width = p.x;
         int height = p.y;
