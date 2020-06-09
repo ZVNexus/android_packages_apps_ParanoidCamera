2c960664d9d7a4bbe2520c2b81f54260ea5a89f2
jiaoyuan <jiaoyuan@codeaurora.org>
2020-06-09 22:31:10 +0800

    Snapcam: add ae lock to capture builder
    add ae lock to capture builder when ae is locked
    
    Change-Id: Ib62b73fc962c0e095b4f67646e79993d6e3c83b7

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -2907,6 +2907,9 @@ public class CaptureModule implements CameraModule, PhotoController,
             CaptureRequest.Builder captureBuilder = getRequestBuilder(
                     CameraDevice.TEMPLATE_STILL_CAPTURE,id);
 
+            if(mLockAFAE){
+                applySettingsForLockExposure(captureBuilder, id);
+            }
             if (mSettingsManager.isZSLInHALEnabled() || isActionImageCapture()) {
                 captureBuilder.set(CaptureRequest.CONTROL_ENABLE_ZSL, true);
             } else {
