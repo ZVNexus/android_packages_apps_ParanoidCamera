e0b6b5d836fce98146c9ea5684fbad2912884acb
jiaoyuan <jiaoyuan@codeaurora.org>
2020-06-17 10:05:16 +0800

    Snapcam: use preview builder when HFR
    use the mVideoRecordRequestBuilder when pause recording
    at the HFR mode
    
    Change-Id: Ia6a9de342b03eabf66f42a79c4c8db1134e2ed93

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -7479,7 +7479,7 @@ public class CaptureModule implements CameraModule, PhotoController,
 
     private void applyZoomAndUpdate(int id) {
         CaptureRequest.Builder captureRequest = mPreviewRequestBuilder[id];
-        if (mMediaRecorderPausing) {
+        if (mMediaRecorderPausing && mCurrentSceneMode.mode != CameraMode.HFR) {
             Log.i(TAG,"update zoom for recording pause state");
             captureRequest = mVideoPreviewRequestBuilder;
         }
